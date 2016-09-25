4990d6a7fead9bda01015b335d851432d91454e7
Jared Thomas <jjthom87@yahoo.com>
2016-09-25 15:58:32 -0400

    start of project, got most of passport working

---
+++ server.js
@@ -0,0 +1,142 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+//var myConnection = require('./config/connection');
+var exphbs = require('express-handlebars');
+var bcrypt = require('bcrypt-nodejs');
+var passport = require('passport');
+var session = require('express-session');
+// have to pass on a Store object on to the session
+var SequelizeStore = require('connect-session-sequelize')(session.Store);
+// using local strategy, and setting it up here to give options.
+var mysql = require('mysql');
+var LocalStrategy = require('passport-local').Strategy;
+var _ = require('underscore');
+var bcrypt = require('bcryptjs');
+
+// this is used to sync the data
+var models = require('./models');
+var db = models.sequelize;
+
+db.sync();
+
+var app = express();
+
+passport.serializeUser(function(user,done){
+  done(null, user);
+ });
+
+passport.deserializeUser(function(obj,done){
+  done(null, obj);
+ });
+
+module.exports = 
+passport.use('local', new LocalStrategy(
+  function(username, password, done){
+    models.User.findOne({ where: {username: username}}).then(function(user){
+        if (!user){
+          return done(null, false, {message: 'Incorrect Username'});
+        }
+        if (!bcrypt.compareSync(password, user.get('password_hash'))){
+          return done(null, false, {message: 'incorrect password'});
+        }
+        return done(null, user)
+      });
+    }
+));
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(session({
+ secret: 'jobtroll is the ticket to success',
+ resave: true,
+ saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//   /set up static files
+// app.use('/static', express.static('public/assets'));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// ------------------------------------
+// ROUTES
+// ------------------------------------
+
+//  ----- Log In  GET Request-------- //
+ app.get('/', function (req, res) {
+    res.render('mainpage');
+  });
+
+ app.get('/login', function(req, res) {
+   res.render('login');
+ });
+
+  app.get('/home', function (req, res){
+        if (!req.isAuthenticated()){
+            req.session.error = 'Please sign in!';
+            res.redirect('/login');
+            return false;
+          };
+          models.User.findOne({ where: {id: req.user.id}}).then(function(user){
+          var data = {
+            user: req.user
+          }
+          res.render('home', {data: data});
+        });
+  });
+
+
+  app.post('/users/login', 
+    passport.authenticate('local', {
+      successRedirect: '/home',
+	    failureRedirect: '/login'
+    })
+  );
+
+// ----- Registration GET Request ------ //
+  app.get('/register', function(req, res) {
+   	res.render('register'); // uses register.handlebars
+  });
+
+     //Register user
+  app.post('/users/create',function(req,res){
+      models.User.create({
+        username: req.body.userName,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+      }).then(function() {
+        res.redirect('/');
+      }).catch(function(err){
+        throw err;
+      });
+  });
+
+
+ app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+ });
+
+// also want to give the option to login via Linkedin
+//passport.use(new LinkedInStrategy({
+//    consumerKey: 77d7l76s8dsyh4,
+//    consumerSecret: CkQizFeB4onJWAAH,
+//    callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
+//  },
+//  function(token, tokenSecret, profile, done) {
+//    User.findOrCreate({ linkedinId: profile.id }, function (err, user) {
+//      return done(err, user);
+//    });
+//  }
+//));
+
+var PORT = process.env.PORT || 8000;
+
+app.listen(PORT, function () {
+  console.log('database operation on port: ' + PORT);
+});
