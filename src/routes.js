4bc88ebaf1b711a3f9d6cd6632f8281c7d444ad5
Eric Mok <EricMok@Erics-MBP.home>
2016-10-02 00:41:45 -0400

    react work in progress login not working yet

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Route } from 'react-router';
+
+// Load this component for ALL routes
+import Application from './Components/app';
+
+// lists ALL todo items
+import TodosListItem from './Components/TodosListItem';
+import MainPage from './Components/MainPage';
+
+export default (
+	/* This means the Application component 
+		is gonna be the parent of all components nested w/in this route! */
+	<Route component={Application}>
+
+		{/* when the address bar shows /#/, render the IndexPage component */}
+
+		<Route path="/todoslistitem" component={TodosListItem} />
+		<Route path="/mainpage" component={MainPage} />
+	</Route>
+);
