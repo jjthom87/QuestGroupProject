0c3cb33f194e3e8aeee7503487dd69b6b72f7b1f
Jared Thomas <jjthom87@yahoo.com>
2016-10-12 07:15:48 -0400

    fixed bundling problem

--- server.js
+++ server.js
@@ -20,145 +20,12 @@ var db = models.sequelize;
 db.sync();
 
 var app = express();
+
 app.use(express.static('public'));
-
 app.use(express.static('src/assets'));
-
-// passport.serializeUser(function(user,done){
-//   done(null, user);
-//  });
-
-// passport.deserializeUser(function(obj,done){
-//   done(null, obj);
-//  });
-
-// module.exports = 
-// passport.use('local', new LocalStrategy(
-//   function(username, password, done){
-//     models.User.findOne({ where: {username: username}}).then(function(user){
-//         if (!user){
-//           return done(null, false, {message: 'Incorrect Username'});
-//         }
-//         if (!bcrypt.compareSync(password, user.get('password_hash'))){
-//           return done(null, false, {message: 'incorrect password'});
-//         }
-//         return done(null, user)
-//       });
-//     }
-// ));
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-// app.use(cookieParser())
-// app.use(session({
-//  secret: 'jobtroll is the ticket to success',
-//   store: new SequelizeStore({
-//    db: db
-//  }),
-//  resave: true,
-//  saveUninitialized: true
-// }));
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-  // app.get('/home', function (req, res){
-  //       models.User.findOne({ where: {username: userInfo.username}}).then(function(currentUser){
-  //         currentUser.getDreams().then(function(dreams){
-  //           var enteredDreams = [];
-
-  //           dreams.forEach(function(dream){
-  //             enteredDreams.push(dream);
-  //           })
-  //         var data = {
-  //           currentUser: currentUser,
-  //           dreams: enteredDreams
-  //         }
-  //         res.json(data);
-  //       });
-  //    });
-  // });
-
-  // app.post('/users/login', 
-  //   passport.authenticate('local', {
-  //     successRedirect: '/home',
-   //    failureRedirect: '/'
-  //   })
-  // );
-
-// ----- Registration GET Request ------ //
-//   app.get('/register', function(req, res) {
-//     res.render('register'); // uses register.handlebars
-//   });
-
-// app.post('/users/login', function(req,res) {
-//   var body = _.pick(req.body, 'username', 'password');
-
-//   if(typeof body.username !== 'string' || typeof body.password !== "string") {
-//     return res.status(400).send();
-//   }
-//   models.User.findOne({
-//     where: {
-//       username: body.username
-//     }
-//   }).then(function(user){ 
-//     if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))){
-//       return res.status(401).send();
-//     }
-//     userInfo = user;
-//     res.json(user);
-//   }, function(e){
-//     res.status(500).send();
-//   })
-// });
-
-//      //Register user
-//   app.post('/users/create',function(req,res){
-//       models.User.create({
-//         firstname: req.body.firstname,
-//         lastname: req.body.lastname,
-//         username: req.body.username,
-//         email: req.body.email,
-//         password: req.body.password
-//       }).then(function(success) {
-//         res.json(success);
-//       }).catch(function(err){
-//         res.json(err);
-//       });
-//   });
-
-//   app.post('/dream/create', function(req, res){
-//     models.User.findOne({where: {username: userInfo.username}}).then(function(){
-//         models.Dream.create({
-//             title: req.body.title,
-//             description: req.body.description,
-//             active: false
-//         }).then(function(dream){
-//         userInfo.addDream(dream).then(function(success){
-//         res.json(dream);
-//       }).catch(function(err){
-//         throw err;
-//       });
-//     })
-//   })
-// });
-
-// app.put('/dream/delete/:id', function(req, res){
-//   models.User.findOne({where: {username: userInfo.username}}).then(function(){
-//     models.Dream.update(
-//     {
-//       active: true
-//     },
-//     {
-//     where: {
-//         id: req.params.id
-//       }
-//     }).then(function(success){
-//       res.json(success);
-//     }).catch(function(err){
-//       throw err;
-//     })
-//   })
-// })
 
 app.post('/mission/create', function(req, res){
   models.Mission.create({
@@ -190,17 +57,6 @@ app.put('/api/task/:id', (req, res) => {
     }
   });
 });
-
-
-
-
-
-
-//  app.get('/logout', function(req, res){
-//   req.logout();
-//   req.session.destroy();
-//   res.redirect('/');
-//  });
 
 var PORT = process.env.PORT || 8000;
 
