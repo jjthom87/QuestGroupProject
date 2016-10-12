443d230dcf771c6b5aa7af6f94e292da7c88f110
Eric Mok <EricMok@Erics-MBP.home>
2016-10-11 23:26:58 -0400

    restructruing component directories

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 
 import Layout from "./pages/layout";
-import MissionMain from "./components/missionmain";
-import QuestMain from "./components/questmain";
+import MissionMain from "./components/mission/missionmain";
+import QuestMain from "./components/quest/questmain";
 
-import MissionsListHeader from './components/missions-list-header';
-import MissionsListItem from './components/missionsListItem';
-import CreateMission from './components/create-mission';
-import MissionsList from './components/missions-list';
+import MissionsListHeader from './components/mission/missions-list-header';
+import MissionsListItem from './components/mission/missionsListItem';
+import CreateMission from './components/mission/create-mission';
+import MissionsList from './components/mission/missions-list';
 
-import QuestsListHeader from './components/quests-list-header';
-import QuestsListItem from './components/questsListItem';
-import CreateQuest from './components/create-quest';
-import QuestsList from './components/quests-list';
-import QuestTitle from './components/quest-title';
+import QuestsListHeader from './components/quest/quests-list-header';
+import QuestsListItem from './components/quest/questsListItem';
+import CreateQuest from './components/quest/create-quest';
+import QuestsList from './components/quest/quests-list';
+import QuestTitle from './components/quest/quest-title';
 
 const app = document.getElementById('app');
 
