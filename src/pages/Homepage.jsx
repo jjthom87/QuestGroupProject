b229f976ad4d416433041c03b0cf445f40297a03
Jared Thomas <jjthom87@yahoo.com>
2016-10-13 18:37:39 -0400

    homepage

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link, IndexLink } from "react-router";
+
+// import Footer from "../components/layout/Footer";
+// import Nav from "../components/layout/Nav";
+
+export default class Homepage extends React.Component {
+  render() {
+    	return (
+      		<div>
+      			<h1>Welcome Home</h1>
+      		</div>
+    	);
+  	}
+}
