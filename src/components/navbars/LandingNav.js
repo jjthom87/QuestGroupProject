4657f1f0e26eaf784af4ba3691584f502ad8fe63
Ethan Favia <efavia215@live.com>
2016-10-13 20:50:23 -0400

    made nav bar a component and got the landing nav bar up and running

--- src/components/navbars/LandingNav.js
+++ src/components/navbars/LandingNav.js
@@ -1,26 +1,37 @@
 var React = require('react'); 
+var {Link, IndexLink} = require('react-router');
 
 var LandingNav = React.createClass({
     render: function () {
         return (
             <header>
-            <nav className="navbar-default navbar-fixed-top clearfix" id="mainNav">
-                <div className="navbar-header">
+                <nav className="navbar navbar-default navbar-fixed-top clearfix" role="navigation" id="mainNav">
+				<div className="container">
+					<div className="navbar-header">
                         <a href="" className="navbar-brand"><img src="#" alt="#" className="img-responsive"/></a>
-                        <h1 id="siteName" className="navbar">Bubo</h1>
-                    </div>
-                <div className='navbar-right'id="signButtons">
-                    <Link to="/register" className="btn btn-primary">Sign Up</Link>
-                    <Link to="/login" className="btn btn-primary">Login</Link>
-                </div>
-                <div class="collapse navbar-collapse main-nav" id="">
-						<ul class="nav navbar-nav navbar-right">
-							<Link to="/register" className="btn btn-primary">Sign Up</Link>
-                            <Link to="/login" className="btn btn-primary">Login</Link>
+                            <h1 id="siteName" className="navbar">Bubo</h1>
+						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bubo-main-nav">
+							<span className="sr-only">Toggle navigation</span>
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+						</button>
+						
+					</div>
+					
+					
+					<div className="collapse navbar-collapse main-nav" id="bubo-main-nav">
+						<ul className="nav navbar-nav navbar-right" id="logonli">
+							<li><Link to="/register" className="landNavitem">Sign Up</Link></li>
+							<li className="landNavitem"><Link to="/login" >Login</Link></li>
+							
 						</ul>
+					</div>
 				</div>
-            </nav>
-        </header>
+			</nav>
+            </header>
         )
     }
-})
+});
+
+module.exports = LandingNav; 
