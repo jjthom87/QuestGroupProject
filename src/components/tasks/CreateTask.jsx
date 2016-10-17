c338904952c107de604867d683a2dac3189fd21c
Jared Thomas <jjthom87@yahoo.com>
2016-10-16 21:28:12 -0400

    got tasks working

--- src/components/tasks/CreateTask.jsx
+++ src/components/tasks/CreateTask.jsx
@@ -5,25 +5,47 @@ export default class CreateTask extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            mission: ''
         };
     }
+    // handleDropdownChange(e){
+    //     this.setState({
+    //         dropdownItem: e.target.value
+    //     })
+    // }
     handleCreateTask(event) {
         event.preventDefault();
 
-        const taskInput = this.refs.taskInput.value;
+        const task = this.refs.task.value;
 
-        if (taskInput.length > 0) {
-            this.refs.taskInput.value = '';
+        if (task.length > 0) {
+            this.refs.task.value = '';
         }
-        this.props.createTask(taskInput);
+        this.props.createTask(task);
     }
     render() {
+        // const { missions } = this.props; 
+
+        // var renderMissionDropdown = () => {
+        //     if (missions.length === 0){
+        //         return (
+        //             <div className="dropdown open" aria-labelledby="dropdownMenuLink">
+        //                 <li className="dropdown-item">No Missions to Add Task To</li>
+        //             </div>
+        //         );
+        //     }
+        //     return missions.map((mission, index) => {
+        //         return (
+        //             <option value={mission.title} className="dropdown-item">{mission.title}</option>
+        //         );
+        //     });
+        // }
+        const { dropdownItem } = this.props
         var renderTaskForm = () => {
                 return (
                     <div>
                         <form onSubmit={this.handleCreateTask.bind(this)}>
-                            <input type="text" placeholder="Create Task" ref="taskInput" />
+                            <input type="text" placeholder="Create Task For Mission" ref="task" />
+                            <input type="hidden" value={dropdownItem} placeholder="Mission Name"/>
                             <input type="submit" value="Add Task"/>
                         </form>
                     </div>
