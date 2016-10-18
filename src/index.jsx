3ff36a92970543059d0db3e02ca03238fc712097
Eric Mok <EricMok@Erics-MBP.home>
2016-10-18 15:51:03 -0400

    Added comments to components: index, routes; WIP

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,16 @@
+// FRONT-END: React Router
+// Rendering our Router Component with {routes} component; Passing down {browser history}
+
+// DEPENDENCIES================================================================
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-
-// NOTE: this is where we "import" our routes from our routes file
+// Importing all of the routes contained in the {routes} component
 import routes from 'routes';
 
-// our entry point to our application! we render our application and "mount"
-// it into the DOM
+
+// ENTRY POINT (to application)==================================================
+// Rendering application and mounting to DOM
 ReactDOM.render(
     <Router history={browserHistory}>{routes}</Router>,
   document.getElementById('app')
