4e80f84a6e882cae2a5397294fa4044052cd5fc2
Ethan Favia <efavia215@live.com>
2016-10-19 19:11:31 -0400

    mission div rendering next to creation form just have to gen form current existing tasks not showing but new ones render to it

---
+++ src/components/mission/MissionListforMM.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import MissionsListHeader from 'MissionsListHeader';
+import MissionsListItem from 'MissionsListItem';
+import MissionAndTaskItem from 'MissionAndTaskItem';
+
+export default class MissionListforMM extends React.Component {
+    render() {
+        const { missions, tasks, toggleTask, deleteMission, deleteTask } = this.props;
+        
+        var renderMissions = () => {
+            return missions.map((mission, index) => {
+                return (
+                    <MissionAndTaskItem
+                        title={mission.title}
+                        description={mission.description}
+                        deleteMission={deleteMission}
+                        deleteTask={deleteTask}
+                        toggleTask={toggleTask}
+                        tasks={tasks}
+                        id={mission.id}
+                        key={index}
+                    />
+                );
+            });
+        }
+        return (
+            <div>
+                <p className="missionsTitle">Missions</p>
+                {renderMissions()}
+            </div>
+        );
+    } 
+}
