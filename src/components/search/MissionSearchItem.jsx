11ec1f6e85ae164f8ec582d53fbf455625497dbc
Eric Mok <emok032@gmail.com>
2016-10-20 16:04:34 -0400

    Keep getting Error- cb is not a function FYI; Also added some logic to search components WIP

--- src/components/search/MissionSearchItem.jsx
+++ src/components/search/MissionSearchItem.jsx
@@ -1,1 +1,40 @@
 // MissionTasks (here)
+import React, { Component } from 'react'; 
+import MissionAndTaskItem from 'MissionAndTaskItem';
+
+export default class MissionSearchItem extends React.Component {
+    render() {
+        const { missions, missiontasks, toggleMissionTask, deleteMission, deleteMissionTask } = this.props;
+        
+        var renderMissions = () => {
+            return missions.map((mission, index) => {
+                return (
+                    <MissionAndTaskItem
+                        title={mission.title}
+                        description={mission.description}
+                        deleteMission={deleteMission}
+                        deleteMissionTask={deleteMissionTask}
+                        toggleMissionTask={toggleMissionTask}
+                        missiontasks={missiontasks}
+                        id={mission.id}
+                        key={index}
+                    />
+                );
+            });
+        }
+        var noMissions = () => {
+            if (missions.length === 0){
+                return (
+                    <p className="noMissionsText">Select a Mission from the Dropdown</p>
+                );
+            }
+        }
+        return (
+            <div>
+                <p className="missionsTitle">Missions</p>
+                {noMissions()}
+                {renderMissions()}
+            </div>
+        );
+    } 
+}
