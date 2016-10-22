2ff3e5734e674663b2aa7fe8d006de6968997330
Jared Thomas <jjthom87@yahoo.com>
2016-10-22 10:04:51 -0400

    got milestonetasks to list with milestones

--- src/components/quest/MilestoneAndTaskItem.jsx
+++ src/components/quest/MilestoneAndTaskItem.jsx
@@ -12,28 +12,47 @@ export default class MilestoneAndTaskItem extends React.Component {
 	render(){
 
 		const { id, title, dateQuest, deleteQuest, completeQuest, deleteMilestone, milestones, taskCompleted, milestonetasks, deleteMilestoneTask, toggleMilestoneTask } = this.props;
-		var singleMilestoneTask = () => {
-			milestonetasks.map((milestonetask, index) => {
-				var milestoneTaskClassName = milestonetask.taskCompleted ? 'task-completed' : 'task-notCompleted';
-				return (
-					<div>
-						<li>
-							<input
-					  			type="checkbox"
-					  			checked={taskCompleted}
-					  			onChange={() => toggleMilestoneTask(milestonetask.uuid)}
-				  			/>
-							<p className={milestoneTaskClassName}>{milestonetask.task}</p>
-							<button onClick={() => deleteMilestoneTask(milestonetask.uuid)}>X</button>
-						</li>
-					</div>
-				)
-			})
-		}
+		// var singleMilestoneTask = () => {
+		// 	milestonetasks.map((milestonetask, index) => {
+		// 		console.log(milestonetask);
+		// 		var milestoneTaskClassName = milestonetask.taskCompleted ? 'task-completed' : 'task-notCompleted';
+		// 		return (
+		// 			<div>
+		// 				<ul>
+		// 					<li>
+		// 						<input
+		// 				  			type="checkbox"
+		// 				  			checked={taskCompleted}
+		// 				  			onChange={() => toggleMilestoneTask(milestonetask.uuid)}
+		// 			  			/>
+		// 						<p className={milestoneTaskClassName}>{milestonetask.task}</p>
+		// 						<button onClick={() => deleteMilestoneTask(milestonetask.uuid)}>X</button>
+		// 					</li>
+		// 				</ul>
+		// 			</div>
+		// 		)
+		// 	})
+		// }
+		const renderMilestonetasks = milestonetasks.map(milestonetask => {
+			var milestoneTaskClassName = milestonetask.taskCompleted ? 'task-completed' : 'task-notCompleted';
+			return (
+				<div>
+					<li>
+						<input
+							type="checkbox"
+							checked={taskCompleted}
+							onChange={() => toggleMilestoneTask(milestonetask.uuid)}
+						/>
+						<p className={milestoneTaskClassName}>{milestonetask.task}</p>
+						<button onClick={() => deleteMilestoneTask(milestonetask.uuid)}>X</button>
+					</li>
+				</div>
+			)
+		})
 		return (
 			<div>
 				<p>Tasks</p>
-				<p>{singleMilestoneTask()}</p>
+				<ul>{renderMilestonetasks}</ul>
 			</div>
 		)
 	}
