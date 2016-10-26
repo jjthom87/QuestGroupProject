a5310ca720cb0f3c552c719343ab862486d8740e
Ethan Favia <efavia215@live.com>
2016-10-26 13:31:58 -0400

    searchbar mission styling

--- src/components/search/searchBar/searchBarMissionItem.jsx
+++ src/components/search/searchBar/searchBarMissionItem.jsx
@@ -36,9 +36,26 @@ export default class SearchBarMissionItem extends React.Component {
         var singleUser = () => {
             return users.map((user, index) => {
                 return (
-                    <span>
-                        <p key={index} id="userText">{user.username}</p>
-                        <img src={user.profileImage} style={{width: 40, height: 40}}/>
+                    <div className="row">
+                        <div className="col-xs-2 col-md-2">
+                            <img src={user.profileImage} style={{width: 100, height: 100}}/>
+                        </div>
+                        <div className="col-xs-6">   
+
+                        <p key={index} className="searchResults"><strong>UserName:</strong>{user.username}</p>
+                        <p className="searchResults"><strong>Start Date: </strong>{missionCreatedOn}</p>
+                        <p className="searchResults"><strong>Likes: </strong>{missionLikes}</p>
+                        </div>
+                    </div>
+                );
+            });
+        }
+
+        var picture = () => {
+            return users.map((user, index) => {
+                return (
+                    <span id="userImage">
+                        <img src={user.profileImage} style={{width: 30, height: 30}}/>
                     </span>
                 );
             });
@@ -48,15 +65,14 @@ export default class SearchBarMissionItem extends React.Component {
             <div className="panelback userprofilediv" id={"panel" + id}>
                  <div className="panel-heading topPanel">
                     <span className="searchPanelHeader"> <a data-toggle="collapse" data-target={"#mcollapse" + id} 
-                    href={"#mcollapse" + id}><strong >Mission: </strong>{missionTitle}</a></span>
+                    href={"#mcollapse" + id}>{picture()}<strong >Mission: </strong>{missionTitle}</a></span>
                 </div>
                 <div id={"mcollapse" + id}className="panel-collapse collapse">
                     <div className="panel-body">
-                        <div className="searchRow"><strong>Description: </strong><p className="searchResults">{missionDescription}</p></div>
-                        <div className="searchRow"><strong>Username: </strong><div className="searchResults">{singleUser()}</div></div>
-                        <div className="searchRow"><strong>Start Date: </strong><p className="searchResults">{missionCreatedOn}</p></div>
-                        <div className="searchRow"><strong>Likes: </strong><p className="searchResults">{missionLikes}</p></div>
+                        {singleUser()}
                         <div className="searchRow"><strong>Status: </strong><p className="searchResults">{missionStatus()}</p></div>
+                        <div className="searchRow"><strong>Description: </strong><p className="searchResults">{missionDescription}</p></div>
+                        
                         <div className="searchRow"><strong>Tasks: </strong><div className="searchResults">{singleTask()}</div></div>
                     </div>
                 </div>
